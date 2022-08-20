a70489b644f18bbd330cbc4e254c0f1ac3d425f2
Miharu Naruse <nars024@gmail.com>
2022-08-20 18:44:19 +0900

    Initial Commit

---
+++ Shared/SwiftUI100KnocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI100KnocksApp.swift
+//  Shared
+//
+//  Created by 成瀬 未春 on 2022/08/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI100KnocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
