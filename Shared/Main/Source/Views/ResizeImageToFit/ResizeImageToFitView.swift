f934e92c76bb54c01d8b550a71b9953beba1587f
Miharu Naruse <nars024@gmail.com>
2022-08-24 23:50:53 +0900

    :sparkles: feat: Update ResizeImageToFitView by loading network image

--- Shared/Main/Source/Views/ResizeImageToFit/ResizeImageToFitView.swift
+++ Shared/Main/Source/Views/ResizeImageToFit/ResizeImageToFitView.swift
@@ -5,11 +5,26 @@
 //  Created by MiharuNaruse on 2022/08/24.
 //
 
+// https://developer.apple.com/documentation/swiftui/fitting-images-into-available-space
+// https://github.com/SDWebImage/SDWebImageSwiftUI#using-webimage-to-load-network-image
+
+import SDWebImageSwiftUI
 import SwiftUI
 
 struct ResizeImageToFitView: View {
+    private let imageURLString: String = "https://placekitten.com/300/300"
+
     var body: some View {
-        Text("Hello, world!")
+        WebImage(url: URL(string: imageURLString))
+            .onSuccess { image, data, cacheType in
+            }
+            .resizable()
+            .placeholder(Image(systemName: "photo"))
+            .indicator(.activity)
+            .transition(.fade(duration: 0.5))
+            .scaledToFit()
+            .frame(width: 150, height: 200, alignment: .center)
+            .background(.red)
     }
 }
 
