1d84d12475d2c1f72c13ddb833c4fcdca6c32944
Miharu Naruse <nars024@gmail.com>
2022-08-24 23:06:19 +0900

    :sparkles: feat: Update ResizeImageToFitView

--- Shared/Main/Source/Views/ResizeImageToFit/ResizeImageToFitView.swift
+++ Shared/Main/Source/Views/ResizeImageToFit/ResizeImageToFitView.swift
@@ -5,4 +5,16 @@
 //  Created by MiharuNaruse on 2022/08/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct ResizeImageToFitView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+struct ResizeImageToFitView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResizeImageToFitView()
+    }
+}
