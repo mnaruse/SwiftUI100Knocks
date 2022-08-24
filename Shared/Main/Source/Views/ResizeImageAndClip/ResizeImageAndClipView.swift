f312dafb924d2e73848e4ca8948ba980328de622
Miharu Naruse <nars024@gmail.com>
2022-08-25 00:26:37 +0900

    :sparkles: feat: Add ResizeImageAndClipView

---
+++ Shared/Main/Source/Views/ResizeImageAndClip/ResizeImageAndClipView.swift
@@ -0,0 +1,20 @@
+//
+//  ResizeImageAndClipView.swift
+//  SwiftUI100Knocks (iOS)
+//
+//  Created by MiharuNaruse on 2022/08/25.
+//
+
+import SwiftUI
+
+struct ResizeImageAndClipView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+struct ResizeImageAndClipView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResizeImageAndClipView()
+    }
+}
