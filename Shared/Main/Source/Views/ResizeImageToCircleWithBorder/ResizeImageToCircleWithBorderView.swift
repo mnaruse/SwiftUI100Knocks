86936d6f2f6ff89baa28932a1b33be93c81831c2
Miharu Naruse <nars024@gmail.com>
2022-08-26 08:43:33 +0900

    :sparkles: feat: Update ResizeImageToCircleWithBorderView

--- Shared/Main/Source/Views/ResizeImageToCircleWithBorder/ResizeImageToCircleWithBorderView.swift
+++ Shared/Main/Source/Views/ResizeImageToCircleWithBorder/ResizeImageToCircleWithBorderView.swift
@@ -5,11 +5,25 @@
 //  Created by MiharuNaruse on 2022/08/26.
 //
 
+// https://developer.apple.com/documentation/swiftui/view/clipshape(_:style:)
+
+import SDWebImageSwiftUI
 import SwiftUI
 
 struct ResizeImageToCircleWithBorderView: View {
+    private let imageURLString: String = "https://placekitten.com/300/300"
+
     var body: some View {
-        Text("Hello, world!")
+        WebImage(url: URL(string: imageURLString))
+            .onSuccess(perform: { _, _, _ in
+
+            })
+            .placeholder(Image(systemName: "photo"))
+            .indicator(.activity)
+            .transition(.fade(duration: 0.5))
+            .scaledToFill()
+            .frame(width: 150, height: 150, alignment: .top)
+            .clipShape(Circle())
     }
 }
 
