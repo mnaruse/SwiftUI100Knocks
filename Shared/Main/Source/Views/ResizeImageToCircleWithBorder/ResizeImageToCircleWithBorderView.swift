5424168a2a4251e65cbe783c39ee1d44090b0355
Miharu Naruse <nars024@gmail.com>
2022-08-26 08:19:31 +0900

    :sparkles: feat: Add ResizeImageToCircleWithBorderView

---
+++ Shared/Main/Source/Views/ResizeImageToCircleWithBorder/ResizeImageToCircleWithBorderView.swift
@@ -0,0 +1,20 @@
+//
+//  ResizeImageToCircleWithBorderView.swift
+//  SwiftUI100Knocks (iOS)
+//
+//  Created by MiharuNaruse on 2022/08/26.
+//
+
+import SwiftUI
+
+struct ResizeImageToCircleWithBorderView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+struct ResizeImageToCircleWithBorderView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResizeImageToCircleWithBorderView()
+    }
+}
