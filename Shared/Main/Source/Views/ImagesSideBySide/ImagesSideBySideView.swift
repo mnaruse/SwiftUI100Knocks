597674611cce17e3d9213b8c8471bcc1f0563ce5
Miharu Naruse <nars024@gmail.com>
2022-08-26 22:18:29 +0900

    :sparkles: feat: Add ImagesSideBySideView

---
+++ Shared/Main/Source/Views/ImagesSideBySide/ImagesSideBySideView.swift
@@ -0,0 +1,20 @@
+//
+//  ImagesSideBySideView.swift
+//  SwiftUI100Knocks (iOS)
+//
+//  Created by MiharuNaruse on 2022/08/26.
+//
+
+import SwiftUI
+
+struct ImagesSideBySideView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+struct ImagesSideBySideView_Previews: PreviewProvider {
+    static var previews: some View {
+        ImagesSideBySideView()
+    }
+}
