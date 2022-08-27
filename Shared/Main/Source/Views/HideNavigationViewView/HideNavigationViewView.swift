c228afe7292c55296ca55b3f920bb4c2ad300520
Miharu Naruse <nars024@gmail.com>
2022-08-27 11:59:41 +0900

    :sparkles: feat: Add HideNavigationViewView

---
+++ Shared/Main/Source/Views/HideNavigationViewView/HideNavigationViewView.swift
@@ -0,0 +1,20 @@
+//
+//  HideNavigationViewView.swift
+//  SwiftUI100Knocks (iOS)
+//
+//  Created by MiharuNaruse on 2022/08/27.
+//
+
+import SwiftUI
+
+struct HideNavigationViewView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+struct HideNavigationViewView_Previews: PreviewProvider {
+    static var previews: some View {
+        HideNavigationViewView()
+    }
+}
