567ad80865f0bc39d0cb56fe02f7d1088ade9bad
Miharu Naruse <nars024@gmail.com>
2022-08-28 22:44:20 +0900

    :sparkles: feat: Add ShowPickerView

---
+++ Shared/Main/Source/Views/ShowPicker/ShowPickerView.swift
@@ -0,0 +1,20 @@
+//
+//  ShowPickerView.swift
+//  SwiftUI100Knocks (iOS)
+//
+//  Created by 成瀬 未春 on 2022/08/28.
+//
+
+import SwiftUI
+
+struct ShowPickerView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+struct ShowPickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShowPickerView()
+    }
+}
