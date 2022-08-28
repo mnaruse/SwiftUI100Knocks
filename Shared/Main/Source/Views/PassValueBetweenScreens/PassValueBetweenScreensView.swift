fffd1f2e29c5871eb3f58ce8e226433ee85f4d42
Miharu Naruse <nars024@gmail.com>
2022-08-28 22:30:31 +0900

    :sparkles: feat: Update PassValueBetweenScreensView

--- Shared/Main/Source/Views/PassValueBetweenScreens/PassValueBetweenScreensView.swift
+++ Shared/Main/Source/Views/PassValueBetweenScreens/PassValueBetweenScreensView.swift
@@ -5,11 +5,24 @@
 //  Created by 成瀬 未春 on 2022/08/28.
 //
 
+import Domain
 import SwiftUI
 
 struct PassValueBetweenScreensView: View {
+    private let fluits: [Fluit] = [
+        Fluit(text: "Apple"),
+        Fluit(text: "Banana"),
+        Fluit(text: "Orange"),
+        Fluit(text: "Grape"),
+        Fluit(text: "Peach"),
+    ]
+
     var body: some View {
-        Text("Hello, world!")
+        List {
+            ForEach(fluits) { fluit in
+                NavigationLink(fluit.text, destination: PassValueBetweenScreensDetailView(text: fluit.text))
+            }
+        }
     }
 }
 
