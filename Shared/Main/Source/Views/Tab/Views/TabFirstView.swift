fe939852c168b5a401c9eb5e9258634f33be6afb
Miharu Naruse <nars024@gmail.com>
2022-11-15 21:42:58 +0900

    :sparkles: feat: Add subviews

---
+++ Shared/Main/Source/Views/Tab/Views/TabFirstView.swift
@@ -0,0 +1,24 @@
+//
+//  TabFirstView.swift
+//  SwiftUI100Knocks (iOS)
+//
+//  Created by 成瀬 未春 on 2022/11/15.
+//
+
+import SwiftUI
+
+struct TabFirstView: View {
+    var body: some View {
+        ZStack {
+            Color.blue
+            Text("1")
+                .foregroundColor(.white)
+        }
+    }
+}
+
+struct TabFirstView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabFirstView()
+    }
+}
---
+++ Shared/Main/Source/Views/Tab/Views/TabSecondView.swift
@@ -0,0 +1,24 @@
+//
+//  TabSecondView.swift
+//  SwiftUI100Knocks (iOS)
+//
+//  Created by 成瀬 未春 on 2022/11/15.
+//
+
+import SwiftUI
+
+struct TabSecondView: View {
+    var body: some View {
+        ZStack {
+            Color.yellow
+
+            Text("2")
+        }
+    }
+}
+
+struct TabSecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabSecondView()
+    }
+}
