f4a83e97e9da549cecb8e7f639893c709b5d4ca9
Miharu Naruse <nars024@gmail.com>
2022-11-23 20:56:13 +0900

    :sparkles: feat: Add PushFromButtonView

---
+++ Shared/Main/Source/Views/PushFromButton/PushFromButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  PushFromButtonView.swift
+//  SwiftUI100Knocks (iOS)
+//
+//  Created by 成瀬 未春 on 2022/11/23.
+//
+
+import SwiftUI
+
+struct PushFromButtonView: View {
+    var body: some View {
+        Text("Hello, world.")
+    }
+}
+
+struct PushFromButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        PushFromButtonView()
+    }
+}
